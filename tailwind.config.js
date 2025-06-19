f596ac78ad8efde131d107bca3baf0ac26597ee8
Yusri Saad <muhd.yusri@rhbgroup.com>
2025-06-19 15:19:38 +0800

    fix tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,14 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // untuk expo-router
+    "./components/**/*.{js,ts,jsx,tsx}", // kalau ada custom komponen
+    "./screens/**/*.{js,ts,jsx,tsx}", // optional
   ],
-theme: {
-    extend: {
-      padding: {
-        '320': '320px',
-      },
-    },
-  },
-  variants: {
-    extend: {
-      padding: ['responsive'],
-    },
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
   },
   plugins: [],
 };
